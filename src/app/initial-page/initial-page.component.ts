b1e6024744b3d3946889537faa094ed3d598673b
Migal <migallewis@gmail.com>
2022-05-28 09:51:25 +0200

    seperated angular templeat into own component

---
+++ src/app/initial-page/initial-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initial-page',
+  templateUrl: './initial-page.component.html',
+  styleUrls: ['./initial-page.component.scss']
+})
+export class InitialPageComponent implements OnInit {
+
+  title = 'angular-bootstrap-template';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
