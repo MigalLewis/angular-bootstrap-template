c67e56086440c4888741974661bff9850e00585a
Migal <migallewis@gmail.com>
2026-05-24 13:05:32 +0200

    upgraded to latest angular

--- src/app/initial-page/initial-page.component.ts
+++ src/app/initial-page/initial-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-initial-page',
+  standalone: false,
   templateUrl: './initial-page.component.html',
   styleUrls: ['./initial-page.component.scss']
 })
